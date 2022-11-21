3cb85b491e7f84079bd3f6b9f1cb170bd6830e03
Valerii Khrolenko <37577958+Jeskay@users.noreply.github.com>
2022-11-21 20:55:55 +1000

    changed application to command line style

--- main.go
+++ main.go
@@ -1,26 +1,38 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	lexer "gocompiler/src"
 	"os"
 	"strings"
 )
 
+var options struct {
+	lex    bool
+	source string
+}
+
 func main() {
-	file, err := os.Open("input.txt")
-	if err != nil {
-		panic(err)
-	}
-	lexerInstance := lexer.NewLexer(file)
-	for {
-		pos, tok, lex, lit := lexerInstance.Lex()
-		if tok == lexer.EOF {
-			break
+	flag.StringVar(&options.source, "source", "input.txt", "filename of source to lex")
+	flag.BoolVar(&options.lex, "lex", false, "perform lexical analysis")
+	flag.Parse()
+
+	if options.lex {
+		file, err := os.Open(options.source)
+		if err != nil {
+			panic(err)
 		}
-		fmt.Printf("%d:%d\t%s\t%s\t%s\n", pos.Line, pos.Column, tok, strings.ReplaceAll(lex, "\r", ""), strings.ReplaceAll(lit, "\r", ""))
-		if tok == lexer.ILLEGAL {
-			break
+		lexerInstance := lexer.NewLexer(file)
+		for {
+			pos, tok, lex, lit := lexerInstance.Lex()
+			if tok == lexer.EOF {
+				break
+			}
+			fmt.Printf("%d:%d\t%s\t%s\t%s\n", pos.Line, pos.Column, tok, strings.ReplaceAll(lex, "\r", ""), strings.ReplaceAll(lit, "\r", ""))
+			if tok == lexer.ILLEGAL {
+				break
+			}
 		}
 	}
 }
